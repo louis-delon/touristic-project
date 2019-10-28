e7bf2740f8cf384db904ac0b191d9038e7cbe4a6
LouisDelon <louleman34@gmail.com>
2019-10-28 21:54:12 +0100

    add I18n gem

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,9 +1,15 @@
 class Site < ApplicationRecord
 
+  # Config =====================================================================
+
+  enum category: %i[restaurant bar shop hiking]
+
   # Associations ===============================================================
+
   belongs_to :sector, inverse_of: :sites
 
   # Validations ================================================================
+
   validates :opening_time, :description, :phone_number, :category, :picture, presence: true
 
 end
