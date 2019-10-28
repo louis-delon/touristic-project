d959e3bca3df5278ab2c1aefe71bd4de821eb4f5
LouisDelon <louleman34@gmail.com>
2019-10-28 08:46:06 +0100

    install devise

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,6 +17,7 @@ require("channels")
 // const imagePath = (name) => images(name, true)
 
 import 'bootstrap'
+import '../stylesheets/application'
 
 document.addEventListener("turbolinks:load", () => {
   $('[data-toggle="tooltip"]').tooltip()
